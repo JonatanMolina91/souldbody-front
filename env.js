e39547e186dfbc5dd5f3f2f65dfab988762b5fcd
Jonatan Molina <battusay000@gmail.com>
2024-06-07 13:49:13 +0200

    modo producion

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
  
  
  export default function getURL() {
-  let develop = true;
+  let develop = false;
     return develop?'http://localhost:8000/':'https://souldbody-337c4235c4cf.herokuapp.com/';
  }
  
